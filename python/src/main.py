49c0eb45f9538d632f2e1643272b6e4f539004fd
Illia Khamza <khamzaillia@gmail.com>
2025-08-30 03:42:14 +0100

    Update CORS

--- python/src/main.py
+++ python/src/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from .api.calculate_endpoints import router as calculate_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",   # React dev server
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",   
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(calculate_router)
 
